584d81f8940c30f5dfd53f71485f1c673213ae53
Otto Kekäläinen <otto@seravo.fi>
2015-04-17 00:31:10 +0300

    Implemented zombie, a light-weight headless browser, for scraping code.org

--- server.js
+++ server.js
@@ -3,11 +3,38 @@
 var express = require('express');
 var app = express();
 
+var Browser = require('zombie');
+var browser = new Browser();
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.sendfile('index.html');
+  // newer syntax: res.sendFile(__dirname + '/index.html');
   var id = parseInt(req.query.id);
   if (typeof id == 'number') {
     console.log('Executing gallery item id ' + id);
+
+    browser.visit('http://studio.code.org/c/42489384/edit', function (e) {
+      if (e) {
+        throw e;
+      }
+
+      /*
+      console.log(browser.text('title'));
+      // wait for new page to be loaded then fire callback function
+      browser.wait().then(function() {
+        console.log(browser.text('title'));
+        console.log(browser.text('#show-code-header'));
+        browser.evaluate("$('#show-code-header').click()");
+        return callback(null);
+      });
+      */
+      // var target = browser.querySelector('#show-code-header');
+      // browser.fire(target, 'click');
+      // console.log(browser.text('title'));
+      // console.log(browser.text('pre.generatedCode'));
+
+    });
+
   }
 });
 
